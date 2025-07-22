890510961d64175ac13ed29032334c998cd08f25
saniyafarzindeveloper <developerfarzin@gmail.com>
2025-07-22 14:14:16 +0530

    completed navbar, main section

---
+++ app/routes/home.tsx
@@ -0,0 +1,32 @@
+import Navbar from "~/components/Navbar";
+import type { Route } from "./+types/home";
+import { resumes } from "../../constants/index";
+import ResumeCard from "~/components/ResumeCard";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "AI-ATS" },
+    { name: "description", content: "AI powered resume reviewing platform" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Reviews</h1>
+          <h2>Review your submission & get feedback backed by AI</h2>
+        </div>
+      </section>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
+}
